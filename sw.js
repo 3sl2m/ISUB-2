dfb5ca3baaa6853d193bfb6d076763ed1524e339
3sl2m <b7000@live.ru>
2026-05-19 22:32:34 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'trpu-pomoshnik-v1';
+const urlsToCache = [
+    'index.html',
+    'manifest.json',
+    'content/images/'
+];
+
+// Установка Service Worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Cache opened');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Перехват запросов
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Возвращаем кэшированный ответ или делаем запрос
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// Обновление кэша
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
